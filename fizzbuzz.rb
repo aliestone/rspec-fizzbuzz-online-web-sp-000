5dcef5e61f2415bc8d8f031533e0119618c9b5c3
Alie Stone <helloaliestone@gmail.com>
2020-09-08 18:05:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
+  def fizzbuzz(n)
+    divisible_by_15(n) and return 'fizzbuzz'
+    divisible_by_3(n)  and return 'fizz'
+    divisible_by_5(n)  and return 'buzz'
+    100
+  end
