cb8a35ad856ff5aae4825b13a933b60b7e9ff3e0
Alie Stone <helloaliestone@gmail.com>
2020-09-08 18:11:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz (number)
-end 
+def fizzbuzz (x)
+  if (x % 3 == 0)
+    puts "Fizz"
+  elsif (x % 5 == 0)
+    puts "Buzz"
+  elsif (x % 15 == 0)
+    puts "FizzBuzz"
+  else puts "nil"
+  end
+end
